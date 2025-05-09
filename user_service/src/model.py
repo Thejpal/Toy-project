076b20294ea956f0ef6e69929ddaace21543dd20
Thejpal <ramannagarithejpal3717@gmail.com>
2025-05-10 01:57:53 +0530

    Removing submodule's files

--- user_service/src/model.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-from uuid import UUID
-from pydantic import BaseModel, Field
-
-class UserModel(BaseModel):
-    user_id: UUID = Field(..., alias = "id")
-    name: str
-    email: Optional[str]
